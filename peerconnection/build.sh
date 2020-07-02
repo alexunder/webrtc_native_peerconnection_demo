bd5aefc6394166058ad725ba8f78eab6ea8a48d0
Alexander Hsu <chenyu.xu@surgnova.com>
2020-07-02 15:25:12 +0800

    Let the peerconnection independently from webrtc code base.

---
+++ peerconnection/build.sh
@@ -0,0 +1,21 @@
+
+set -eu
+
+readonly ROOT_PATH=$(cd $(dirname $0) && pwd)
+
+echo ${ROOT_PATH}
+
+readonly LOCAL_ENV_PATH=${ROOT_PATH}/local
+
+export CC=cc
+export CXX=c++
+
+readonly BUILD_PATH=${ROOT_PATH}/build
+mkdir -p ${BUILD_PATH}
+
+cd ${BUILD_PATH}
+cmake -DLIBWEBRTC_PATH=${LOCAL_ENV_PATH} ..
+make
+
+cp webrtc_client ${ROOT_PATH}
+cp webrtc_server ${ROOT_PATH}
